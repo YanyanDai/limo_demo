a892616ef38ba394b1ff55e5e6a977da971fa2b2
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2022-12-07 20:39:30 +0900

    Update limo_subscriber.py

--- scrpits/limo_subscriber.py
+++ scrpits/limo_subscriber.py
@@ -4,25 +4,24 @@ import rospy
 from nav_msgs.msg  import Odometry
 
 INTRODUCTION = """
----------------------------
-订阅机器人的实时位姿并打印到终端
----------------------------
+------------------------------------
+Subscribe robot's real-time position
+------------------------------------
 """
-# 打印功能信息
+# Print function information
 print(INTRODUCTION)
 
 
-# 回调函数处理消息
+# Callback function to process message
 def limoCallBack(msg):
-    rospy.loginfo("limo pose: x:%06f, y:%0.6f, z:%0.6f",\
-        msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.orientation.z)
-
-# 初始化 ros 节点
-rospy.init_node('limo_subscriber',anonymous = False)
-
-# 初始化机器人位姿的订阅者
-rospy.Subscriber("/odom",\
-    Odometry, limoCallBack, queue_size=1)
     
-# 循环等待信息数据
-rospy.spin()
+
+# Initialize ros Node
+
+
+# Create a subscriber, subscribe the necessary topic;
+
+    
+# Wait for the topic message,
+# and call the callback function after receiving the message;
+
