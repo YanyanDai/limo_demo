c9af5141c411ad1ad225405d2d46f424b4b93955
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2022-12-07 20:09:09 +0900

    Update draw_circle.py

--- scrpits/draw_circle.py
+++ scrpits/draw_circle.py
@@ -16,19 +16,17 @@ print(INTRODUCTION)
 
 
 # Initialize ros node
-rospy.init_node("draw_circle", anonymous=True)
+
 
 # Initialize the control command publisher
-cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
 
 # Initialize Twist control message
-twist = Twist()
-twist.linear.x = 0.3
-twist.angular.z = 1.5
+
 
 # Initialize ros main loop
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     # Publish control command
-    cmd_vel_pub.publish(twist)
+    
     rate.sleep()
